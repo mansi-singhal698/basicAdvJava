13c31d460b6d81657abc4193e3fc207790b73658
Mansi Singhal <singhalmansi817@gmail.com>
2025-08-31 21:52:38 +0530

    add files

---
+++ searching/ceiling.java
@@ -0,0 +1,40 @@
+package searching;
+public class ceiling{
+    public static void main(String[]args){
+        int[]arr = {2,3,5,9,14,16,18};
+        int target = 15;
+        System.out.println(solution(arr,target));
+        System.out.println(solution1(arr,target));
+    }
+    static int solution(int[]arr,int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target<arr[mid]){
+                end = mid-1;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return start;
+    }
+
+    static int solution1(int[]arr,int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target<arr[mid]){
+                end = mid-1;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return end;
+    }
+}
---
+++ searching/findFirstAndLastPos.java
@@ -0,0 +1,44 @@
+package searching;
+import java.util.*;
+public class findFirstAndLastPos{
+    public static void main(String[]args){
+        int[]arr = {5,7,7,8,8,10};
+        int target = 8;
+        int[]ans = searchRange(arr,target);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    static int[] searchRange(int[]nums,int target){
+        int[]ans = {-1,-1};
+        int start = search(nums,target,true);
+        int end = search(nums,target,false);
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+
+    static int search(int[]arr, int target, boolean findStartIndex){
+        int ans = -1;
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target<arr[mid]){
+                end = mid-1;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else{
+                ans = mid;
+                if(findStartIndex){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+
+    
+}
