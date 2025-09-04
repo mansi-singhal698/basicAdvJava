bdf6e29de4bcd30ec2c27b026389fbbc48ab43a4
Mansi Singhal <singhalmansi817@gmail.com>
2025-09-05 00:00:38 +0530

    add files

---
+++ sorting/findAllMissing.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class findAllMissing{
+    public static void main(String[]args){
+        int[]arr = {3,2,3,4,1,2,7,8};
+        System.out.println(sol(arr));
+    }
+    static ArrayList<Integer> sol(int[]arr){
+        int i = 0;
+        while(i<arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int index = 0; index<arr.length; index++) {
+            if(arr[index]!=index+1){
+                ans.add(index+1);
+            }
+        }
+        return ans;
+    }
+    static void swap(int[]arr,int first,int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
---
+++ sorting/setMismatch.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class setMismatch{
+    public static void main(String[]args){
+        int[]arr = {1,2,2,3,4};
+       int[]m = sort(arr);
+       System.out.println(Arrays.toString(m));
+    }
+    static int[] sort(int[]arr){
+        int i = 0;
+        while(i<arr.length){
+            int correct = arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+        for(int index = 0;index<arr.length;index++){
+            if(arr[index]!=index+1){
+                return new int[]{arr[index],index+1};
+            }
+        }
+        return new int[]{-1,-1};
+    }
+    static void swap(int[]arr,int first,int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
